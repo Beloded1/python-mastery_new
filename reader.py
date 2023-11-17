9e8d4c1684f9092960437c6b66e15f5a2511c692
Beloded1 <alexandrbeloded@gmail.com>
2023-11-17 11:47:04 +0400

    Do some fix

--- reader.py
+++ reader.py
@@ -10,6 +10,6 @@ def read_csv_as_dicts(filename, types):
         rows = csv.reader(f)
         headers = next(rows)
         for row in rows:
-            record = { name: func(val) for name, func, val in zip(headers, types, row)}
+            record = { name: func(val) for name, func, val in zip(headers,  types, row)}
             records.append(record)
     return records
